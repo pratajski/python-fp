1fd95756a806397165c384126d26814d25dde6e8
Pawel <pawelrat@poczta.onet.pl>
2022-08-30 15:36:24 -0400

    synchronization packet

--- Network Attacker.py
+++ Network Attacker.py
@@ -1,7 +1,17 @@
-import scapy.all
+from scapy.all import *
 
 target = input("Type target: ")
 
+Registered_Ports = []
+for i in range(1, 1024):
+    Registered_Ports.append(i)
 
+open_ports = []
 
+def scanport(port):
+    source_rand_port = RandShort()
+    return rand_port
+    conf.verb = 0
+    synchronization_packet = sr1(IP(dst=target) / TCP(sport=source_rand_port,
+                                dport=port, flags = "S"), timeout=0.5)
 
