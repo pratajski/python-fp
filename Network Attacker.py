10b1e258bf304393ed26e39838a01cf067dddfb1
PawelRa <pawelrat@poczta.onet.pl>
2022-09-05 10:15:29 +0200

    synchronization_packet

--- Network Attacker.py
+++ Network Attacker.py
@@ -15,6 +15,17 @@ def scanport(port):
                                                       dport=port, flags="S"), timeout=0.5)
 
     if str(type(synchronization_packet)) != "class <'NoneType'>":
-        pass
+        if synchronization_packet.haslayer(TCP):
+            if synchronization_packet.getlayer(TCP).flags == 0x12:
+                print(f'Port: {port} is available')
+                sr(IP(dst=target) / TCP(sport=source_rand_port, dport=port, flags='R'), timeout=2)
+                return True
+            else:
+                print(f'Port: {port} is closed')
+                return False
+        else:
+            return False
     else:
         return False
+
+
