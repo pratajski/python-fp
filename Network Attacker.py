9b6d10f3b6b40c61d573ca080890b62b59b55ab5
PawelRa <pawelrat@poczta.onet.pl>
2022-09-05 09:13:01 +0200

    is synchronization packet?

--- Network Attacker.py
+++ Network Attacker.py
@@ -1,17 +1,20 @@
 from scapy.all import *
+from scapy.layers.inet import TCP, IP
 
 target = input("Type target: ")
 
-Registered_Ports = []
-for i in range(1, 1024):
-    Registered_Ports.append(i)
+Registered_Ports = range(1, 1023)
 
 open_ports = []
 
+
 def scanport(port):
     source_rand_port = RandShort()
-    return rand_port
     conf.verb = 0
     synchronization_packet = sr1(IP(dst=target) / TCP(sport=source_rand_port,
-                                dport=port, flags = "S"), timeout=0.5)
+                                                      dport=port, flags="S"), timeout=0.5)
 
+    if str(type(synchronization_packet)) != "class <'NoneType'>":
+        pass
+    else:
+        return False
